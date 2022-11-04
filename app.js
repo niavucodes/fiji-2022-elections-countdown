6b4488eedf4d53c7eed3724ed55d907a67c405e9
Kiti Niavu <kitkatfj94@gmail.com>
2022-11-04 22:40:21 +1200

    Fiji 2022 Elections Countdown Timer V1
    commit v1 of Fiji 2022 elections countdown timer

---
+++ app.js
@@ -0,0 +1,28 @@
+const days = document.getElementById('days');
+const hours = document.getElementById('hours');
+const minutes = document.getElementById('minutes');
+const seconds = document.getElementById('seconds');
+
+const currentYear = new Date().getFullYear();
+
+const electionDay = new Date('December 14 2022 00:00:00 GMT+12');
+
+//Update Countdowntime
+function updateCountdowntime() {
+    const currentTime = new Date();
+    const diff = electionDay - currentTime;
+
+    const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const h = Math.floor(diff / 1000 / 60 / 60) % 24;
+    const m = Math.floor(diff / 1000 / 60) % 60;
+    const s = Math.floor(diff / 1000) % 60;
+    
+    days.innerHTML = d < 10 ? '0' + d : d;
+    hours.innerHTML = h < 10 ? '0' + h : h;
+    minutes.innerHTML = m < 10 ? '0' + m : m;
+    seconds.innerHTML = s < 10 ? '0' + s : s;
+
+    
+}
+
+setInterval(updateCountdowntime, 1000);
